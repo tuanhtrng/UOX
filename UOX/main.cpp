74304bb8f2bcff862207c64c187c1f18b6b6f265
Charles Kerr <charleskerr59@gmail.com>
2020-04-21 09:27:32 -0400

    Initial Commit

---
+++ UOX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UOX
+//
+//  Created by Charles Kerr on 4/21/20.
+//  Copyright © 2020 Charles Kerr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
