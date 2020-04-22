35448f937147de9c2a51df89b2ac07d35eea1edb
Charles Kerr <charleskerr59@gmail.com>
2020-04-22 10:33:34 -0400

    Investigating stuff

--- UOX/main.cpp
+++ UOX/main.cpp
@@ -5,11 +5,31 @@
 //  Created by Charles Kerr on 4/21/20.
 //  Copyright © 2020 Charles Kerr. All rights reserved.
 //
-
+// System headers
 #include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+// Third party
+#include "json.h"
+
+// Our includes
+#include "ConfFile.hpp"
+#include "StringFunctions.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Make sure we got passed our configuration
+    if (argc < 2) {
+        std::cout <<"Program must be started with a configuration file"<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::string configpath(argv[1]) ;
+    std::ifstream configstream;
+    ConfFile config ;
+    config.loadFile(configpath);
+    
+    
+    
+    
     return 0;
 }
